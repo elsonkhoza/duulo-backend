7041d131465aa2ac6f0d80fcedf1704421da0ee4
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-10 19:56:54 +0200

    create profile model

--- src/main/java/com/duulo/server/profile/Profile.java
+++ src/main/java/com/duulo/server/profile/Profile.java
@@ -1,4 +1,32 @@
 package com.duulo.server.profile;
 
+
+import com.duulo.server.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "profiles")
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    String username;
+
+    @Column(name = "profile_image_url")
+    String profileImageUrl;
+
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    User user;
 }
